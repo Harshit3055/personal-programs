003ea3fb6c5db68c84ac3a502cb848806bee75dc
Harshit Agrawal <76194709+Harshit3055@users.noreply.github.com>
2020-12-20 13:16:04 +0530

    The Whole Program
    Here is The Whole Program.

--- Numbers.java
+++ Numbers.java
@@ -1,7 +1,41 @@
+import java.util.*;
 class Numbers
 {
     public static void main(String args[])
     {
-        System.out.print("Harshit Agrawal is the Best");
+        Scanner in = new Scanner(System.in);
+        int len, num, a;
+        String Out="", Str="";
+        System.out.print("Enter a Number ");
+        num = in.nextInt();
+        do
+        {
+            a = num%10;
+            num/=10;
+
+            if(a==0)
+              Out = "Zero";
+            if(a==1)
+              Out = "One";
+            if(a==2)
+              Out = "Two";
+            if(a==3)
+              Out = "Three";
+            if(a==4)
+              Out = "Four";
+            if(a==5)
+              Out = "Five";
+            if(a==6)
+              Out = "Six";
+            if(a==7)
+              Out = "Seven";
+            if(a==8)
+              Out = "Eight";
+            if(a==9)
+              Out = "Nine";
+            
+            Str = Out +" "+Str;
+        }while(num!=0);
+        System.out.print(Str);
     }
 }
